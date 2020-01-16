cb7dbcc7cd64538814f7b4c6ce0756f519aea6b6
hashmat2526 <hashmatajalali@gmail.com>
2020-01-16 12:00:03 +0500

    adding chalk

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const chalk = require("chalk");
 const request = require("request");
 const mongoose = require("mongoose");
 const connetdb = require("./api/dependencies/connectdb");
