77b96eeb4ff42096308a00c46dcef00c6c25aa54
hashmat2526 <hashmatajalali@gmail.com>
2020-01-16 12:04:50 +0500

    s

--- server.js
+++ server.js
@@ -57,32 +57,36 @@ app.get("/", (req, res) => {
 /* THIS IS A VERYIFICATION ROUTE. THIS WILL BE HIT BY FACEBOOK TO VERIFY */
 
 // Adds support for GET requests to our webhook
-app.get("/webhook", (req, res) => {
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "this_is_a_secret_token";
+app.get("/webhook", async (req, res) => {
+  try {
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "this_is_a_secret_token";
 
-  // Parse the query params
-  let mode = req.query["hub.mode"];
-  let token = req.query["hub.verify_token"];
-  let challenge = req.query["hub.challenge"];
-  console.log("mode \n", mode);
-  console.log("token \n", token);
-  console.log("challenge\n", challenge);
+    // Parse the query params
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+    console.log("mode \n", mode);
+    console.log("token \n", token);
+    console.log("challenge\n", challenge);
 
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-    // Checks the mode and token sent is correct
-    if (mode === "subscribe" && token === VERIFY_TOKEN) {
-      // Responds with the challenge token from the request
-      console.log("WEBHOOK_VERIFIED");
-      res.status(200).send(challenge);
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+      // Checks the mode and token sent is correct
+      if (mode === "subscribe" && token === VERIFY_TOKEN) {
+        // Responds with the challenge token from the request
+        console.log("WEBHOOK_VERIFIED");
+        res.status(200).send(challenge);
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
 
-      console.log("WEBHOOK_IS_NOT_VERIFIED");
+        console.log("WEBHOOK_IS_NOT_VERIFIED");
 
-      res.sendStatus(403);
+        res.sendStatus(403);
+      }
     }
+  } catch (error) {
+    console.log("err in get req ", error);
   }
 });
 
@@ -91,7 +95,7 @@ app.post("/webhook", async (req, res) => {
   try {
     let body = req.body;
     console.log("jal;sdkfjl;");
-    console.log(chalk.green.inverse("body"), body);
+    console.log("\n body \n", body);
 
     // Checks this is an event from a page subscription
     if (body.object === "page") {
@@ -118,7 +122,7 @@ app.post("/webhook", async (req, res) => {
       res.sendStatus(404);
     }
   } catch (error) {
-    console.log(chalk.blue.bold.inverse("\n error in server "));
+    console.log("\n error in server ");
     console.log(error);
   }
 });
