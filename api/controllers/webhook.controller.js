918ff5f7e0e912ba7632d69aaaaea9097e972461
hashmat2526 <hashmatajalali@gmail.com>
2020-01-17 13:02:21 +0500

    modularity of code

--- api/controllers/webhook.controller.js
+++ api/controllers/webhook.controller.js
@@ -5,6 +5,9 @@ const config = require("../dependencies/config");
 
 const verifyWebhook = async (req, res, next) => {
   try {
+    return res.status(200).json({
+      result: "success"
+    });
     // Your verify token. Should be a random string.
     let VERIFY_TOKEN = "this_is_a_secret_token";
 
