f9bd9ecadf468222d219834661509da7955ae961
hashmat2526 <hashmatajalali@gmail.com>
2020-01-18 11:08:46 +0500

    setting dialogflow

--- api/controllers/process-message.js
+++ api/controllers/process-message.js
@@ -27,50 +27,58 @@ const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 // Don't forget to add it to your `variables.env` file.
 const FACEBOOK_ACCESS_TOKEN = PAGE_ACCESS_TOKEN;
 
-const sendTextMessage = (userId, text) => {
-  return fetch(
-    `https://graph.facebook.com/v2.6/me/messages?access_token=${FACEBOOK_ACCESS_TOKEN}`,
-    {
-      headers: {
-        "Content-Type": "application/json"
-      },
-      method: "POST",
-      body: JSON.stringify({
-        messaging_type: "RESPONSE",
-        recipient: {
-          id: userId
+const sendTextMessage = async (userId, text) => {
+  try {
+    return fetch(
+      `https://graph.facebook.com/v2.6/me/messages?access_token=${FACEBOOK_ACCESS_TOKEN}`,
+      {
+        headers: {
+          "Content-Type": "application/json"
         },
-        message: {
-          text
-        }
-      })
-    }
-  );
+        method: "POST",
+        body: JSON.stringify({
+          messaging_type: "RESPONSE",
+          recipient: {
+            id: userId
+          },
+          message: {
+            text
+          }
+        })
+      }
+    );
+  } catch (error) {
+    console.log("err in sendTextMessage ", error);
+  }
 };
 
 const processMessageInDialogFlow = async webhook_event => {
-  /* USER ID AND ITS MESSAGE FROM FACEBOOK CHATBOT */
-  const userId = webhook_event.sender.id;
-  const message = event.message.text;
+  try {
+    /* USER ID AND ITS MESSAGE FROM FACEBOOK CHATBOT */
+    const userId = webhook_event.sender.id;
+    const message = webhook_event.message.text;
 
-  const request = {
-    session: sessionPath,
-    queryInput: {
-      text: {
-        text: message,
-        languageCode: languageCode
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          text: message,
+          languageCode: languageCode
+        }
       }
-    }
-  };
-  sessionClient
-    .detectIntent(request)
-    .then(responses => {
-      const result = responses[0].queryResult;
-      return sendTextMessage(userId, result.fulfillmentText);
-    })
-    .catch(err => {
-      console.error("ERROR:", err);
-    });
+    };
+    sessionClient
+      .detectIntent(request)
+      .then(responses => {
+        const result = responses[0].queryResult;
+        return sendTextMessage(userId, result.fulfillmentText);
+      })
+      .catch(err => {
+        console.error("ERROR:", err);
+      });
+  } catch (error) {
+    console.log("err in sendTextMessage ", error);
+  }
 };
 
 module.exports = {
